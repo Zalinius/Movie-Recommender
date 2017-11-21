4618a767a236ee607d39f641dc91d8f2ff87bcbe
Zachary Lapointe <31998081+Zalinius@users.noreply.github.com>
2017-11-21 14:07:48 -0500

    Made all Term member variables private. Some simple refactoring will be necessary in TaskPrinter.cpp

--- src/Term.cpp
+++ src/Term.cpp
@@ -9,21 +9,12 @@
 
 /**
  * The Constructor for the Term class
- *  @param term The actual stored word
- *  @param docNo The file the word was found in
- *  @param fileAmount the amount of files being processed, for sizing the vectors
+ *  @param term The actual stored word. It should be in lower_case.
  */
-Term::Term(string term, unsigned short docNo, size_t fileAmount)
+Term::Term(string term)
 :term(term)
 {
 	documentFrequency = 1;
-//	termFrequencies.resize(fileAmount);
-//	//weight.resize(fileAmount);
-//	for(vector<unsigned short>::iterator it = termFrequencies.begin(); it != termFrequencies.end(); ++it){
-//		*it = 0;
-//	}
-//
-//	termFrequencies.at(docNo) = 1;
 }
 
 void Term::incrementDocumentFrequency() const{
@@ -42,12 +33,24 @@ const bool operator== (const Term & left, const Term & right){
 }
 
 /**
- * A term comes before another if it's word comes before the other's word alphabetically.
+ * A term comes before another if it's word comes before the other's word alpha-numerically.<br>
+ * Creates a strict, weak ordering system for Term objects.
+ * @param left The term object on the left of the operator.
+ * @param right The term object on the right of the operator.
+ * @return True if the left Term precedes the right Term.<br>
+ * False otherwise.
  */
 const bool operator< (const Term & left, const Term & right){
 	return(left.term < right.term);
 }
 
+/**
+ * An overloading of the insertion operator for debugging purposes.<br>
+ * Inserts information about the word and document frequency into the stream.
+ * @param os The output stream.
+ * @param t The term object being inserted into the stream.
+ * @return The updated output stream.
+ */
 ostream &operator<< (ostream &os, const Term & t){
 	os << t.term << " shows up in ";
 	os << t.documentFrequency;
--- src/Term.h
+++ src/Term.h
@@ -13,26 +13,34 @@ using namespace std;
 #include <string>
 #include <iostream>
 
+/**
+ * A wrapper class for considering terms. It contains the word in question, as well as its document frequency.<br>
+ * Term objects follow a strict weak ordering determined by the alpha-numerical order of their words.
+ */
 class Term{
 
 public:
-	Term(string term, unsigned short docNo, size_t fileAmount);
+	Term(string term);
 
-	string term; //The actual word being represented
-
-	mutable unsigned short documentFrequency; //The document frequency
+	const string& getWord() const;
 
 	void incrementDocumentFrequency() const;	//doesn't change the ordering on the object in the set
 	unsigned short getDocumentFrequency() const;
+
 
 	friend const bool operator== (const Term & left, const Term & right);
 	friend const bool operator< (const Term & left, const Term & right);
 	friend ostream &operator<< (ostream &os, const Term & right);
 
 private:
+	string term; //The actual word being represented
 
+	/**
+	 * This specific member variable is mutable, as we use Term objects in const datastructures, such as set.<br>
+	 * As they are const for ordering reasons, it is ok to mark documentFrequency mutable, as it does not affect the ordering of a Term object.
+	 * @see Set
+	 */
+	mutable unsigned short documentFrequency; //The document frequency
 };
-
-
 
 #endif /* TERM_H_ */
